edfd9a6c81bd0a904c3f15d7de181b43b60c38d7
Polisvh <145322774+Polisvh@users.noreply.github.com>
2024-11-30 11:11:53 +0100

    Update FreezingListener.java

--- core/src/main/java/me/mykindos/betterpvp/core/effects/listeners/effects/FreezingListener.java
+++ core/src/main/java/me/mykindos/betterpvp/core/effects/listeners/effects/FreezingListener.java
@@ -13,8 +13,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @BPvPListener
 @Singleton
